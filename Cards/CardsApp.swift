6468c66250b79a57437f120599023226a57ef310
Maksim Tarasov <112902969+maksyak108@users.noreply.github.com>
2023-06-14 01:11:14 +0300

    Cards by SwiftUI
    Cards by SwiftUI

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Максим Тарасов on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
